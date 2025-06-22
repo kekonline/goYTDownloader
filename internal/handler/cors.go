02424c62e4f486ded1a79eb315cc1f2bd323abf9
Rishi <kekonline@gmail.com>
2025-06-22 12:25:01 +0200

    added .env file and updated CORS handler to load environment variables

--- internal/handler/cors.go
+++ internal/handler/cors.go
@@ -2,13 +2,25 @@
 package handler
 
 import (
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func WithCORS(h http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Load .env file
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+
+		HOST := os.Getenv("HOST")
+
 		// Allow your frontend origin here
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", HOST)
 		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
