9a65bc11734117c77cf0fbe87ce97c99a14620ef
Rishi <kekonline@gmail.com>
2025-06-24 09:29:20 +0200

    refactor audio stream handler to dynamically set filename in response headers

--- internal/handler/audio.go
+++ internal/handler/audio.go
@@ -2,10 +2,12 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os/exec"
+	"strings"
 
 	"goYTDownloader/internal/model"
 )
@@ -25,13 +27,23 @@ func AudioStreamHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := cmd.Start(); err != nil {
-		log.Printf("GOT TO HERe")
 		http.Error(w, "Failed to start yt-dlp", http.StatusInternalServerError)
 		return
 	}
 
+	cmdName := exec.Command("yt-dlp", "--get-filename", "-o", "%(title)s.%(ext)s", req.URL)
+	output, err := cmdName.Output()
+	if err != nil {
+		http.Error(w, "Failed to get filename", http.StatusInternalServerError)
+		return
+	}
+	filename := strings.TrimSpace(string(output))
+	log.Printf("Streaming audio for: %s", filename)
+
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Expose-Headers", "Content-Disposition")
 	w.Header().Set("Content-Type", "audio/webm")
-	w.Header().Set("Content-Disposition", "attachment; filename=\"audio.webm\"")
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
 
 	_, err = io.Copy(w, stdout)
 	if err != nil {
