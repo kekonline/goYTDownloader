3b5204eb3d4ff2c4c316a63bb5d04f76751ffdef
Rishi <kekonline@gmail.com>
2025-07-24 15:16:13 +0200

    update server port from 8080 to 10000 in main.go and Dockerfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,6 @@ func main() {
 	http.HandleFunc("/api/audio-stream", handler.WithCORS(handler.AudioStreamHandler))
 	http.HandleFunc("/api/audio-stream-multiple-files-poc", handler.WithCORS(handler.AudioStreamHandlerMultipleFilesPoc))
 	http.HandleFunc("/api/audio-streamv2", handler.WithCORS(handler.AudioStreamHandlerV2))
-	log.Println("Server running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Server running on :10000")
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
