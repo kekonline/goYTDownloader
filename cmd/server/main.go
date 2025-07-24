3b3a8685d5fed5db4de929af622140a6a5605e79
Rishi <kekonline@gmail.com>
2025-07-24 15:01:46 +0200

    refactor server to retrieve port from environment variables for better deployment flexibility

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"goYTDownloader/internal/handler"
 )
@@ -11,6 +12,21 @@ func main() {
 	http.HandleFunc("/api/audio-stream", handler.WithCORS(handler.AudioStreamHandler))
 	http.HandleFunc("/api/audio-stream-multiple-files-poc", handler.WithCORS(handler.AudioStreamHandlerMultipleFilesPoc))
 	http.HandleFunc("/api/audio-streamv2", handler.WithCORS(handler.AudioStreamHandlerV2))
-	log.Println("Server running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	port := getPort() // get port from Render
+
+	log.Printf("Server running on %s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func getPort() string {
+	port := ":" + getenv("PORT", "8080") // Render sets $PORT automatically
+	return port
+}
+
+func getenv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
 }
